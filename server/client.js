82e35a6e0bddb15af49c2dc851093976b9ad91f0
Anna Goncharova <anna.gonch.liaison@gmail.com>
2020-01-20 18:13:11 -0600

    server added

---
+++ server/client.js
@@ -0,0 +1,5 @@
+const test = [1,2,3]
+
+//const clients = [{}]
+
+module.exports = clients, test
---
+++ server/server.js
@@ -0,0 +1,15 @@
+// const express = require express ()
+
+const clients = require ('./client')
+const test = require
+
+const app = express ()
+const port = process.env.PORT
+
+app.user(bodyParser.json())
+app.user
+
+app.get('/',(req,res) => {
+    console.log('server is connected')
+    
+});
